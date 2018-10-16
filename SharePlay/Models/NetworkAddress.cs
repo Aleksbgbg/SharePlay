2bdaf32b3d7bd0126c29aa4b45aaedb6aaeb253a
Aleksbgbg <kozichka01@abv.bg>
2018-10-16 19:00:53 +0100

    [NetworkAddress] Add model which contains an IP and Port, with a ToString method which returns an '{IP}:{Port}' format.

---
+++ SharePlay/Models/NetworkAddress.cs
@@ -0,0 +1,22 @@
+﻿namespace SharePlay.Models
+{
+    using System.Net;
+
+    internal class NetworkAddress
+    {
+        public NetworkAddress(IPAddress ipAddress, int port)
+        {
+            IpAddress = ipAddress;
+            Port = port;
+        }
+
+        public IPAddress IpAddress { get; }
+
+        public int Port { get; }
+
+        public override string ToString()
+        {
+            return string.Concat(IpAddress, ":", Port);
+        }
+    }
+}
