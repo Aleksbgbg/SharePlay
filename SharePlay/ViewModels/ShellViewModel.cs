8daae0a99619757bf823eb0b04352122ba5de72e
Aleksbgbg <kozichka01@abv.bg>
2018-10-17 16:21:40 +0100

    [ShellViewModel] Use factory and window manager to show host and join server windows in appropriate methods.

--- SharePlay/ViewModels/ShellViewModel.cs
+++ SharePlay/ViewModels/ShellViewModel.cs
@@ -1,13 +1,22 @@
 ﻿namespace SharePlay.ViewModels
 {
+    using Caliburn.Micro;
+
+    using SharePlay.Factories.Interfaces;
     using SharePlay.ViewModels.Interfaces;
 
     internal sealed class ShellViewModel : ViewModelBase, IShellViewModel
     {
-        public ShellViewModel(IMainViewModel mainViewModel)
+        private readonly IConnectionFactory _connectionFactory;
+
+        private readonly IWindowManager _windowManager;
+
+        public ShellViewModel(IConnectionFactory connectionFactory, IWindowManager windowManager, IMainViewModel mainViewModel)
         {
             DisplayName = "SharePlay";
 
+            _connectionFactory = connectionFactory;
+            _windowManager = windowManager;
             MainViewModel = mainViewModel;
         }
 
@@ -15,10 +24,12 @@
 
         public void ShowHostWindow()
         {
+            _windowManager.ShowWindow(_connectionFactory.MakeHostServerViewModel());
         }
 
         public void ShowJoinWindow()
         {
+            _windowManager.ShowWindow(_connectionFactory.MakeJoinServerViewModel());
         }
     }
 }
