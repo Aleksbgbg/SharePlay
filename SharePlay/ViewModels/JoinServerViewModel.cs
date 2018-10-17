edc1ca5f4e7ac88720dc8c4a1d997d48e94ccf9f
Aleksbgbg <kozichka01@abv.bg>
2018-10-17 18:10:44 +0100

    [JoinServerViewModel] Implement 'Connect' method by connecting the PlayClientService to the parsed address.

--- SharePlay/ViewModels/JoinServerViewModel.cs
+++ SharePlay/ViewModels/JoinServerViewModel.cs
@@ -1,13 +1,36 @@
 ﻿namespace SharePlay.ViewModels
 {
+    using SharePlay.Models;
+    using SharePlay.Services.Interfaces;
     using SharePlay.ViewModels.Interfaces;
 
     internal class JoinServerViewModel : ViewModelBase, IJoinServerViewModel
     {
-        public string Status { get; } = "Disconnected";
+        private readonly IPlayClientService _playClientService;
+
+        public JoinServerViewModel(IPlayClientService playClientService)
+        {
+            _playClientService = playClientService;
+        }
+
+        private string _status = "Disconnected";
+        public string Status
+        {
+            get => _status;
+
+            set
+            {
+                if (_status == value) return;
+
+                _status = value;
+                NotifyOfPropertyChange(() => Status);
+            }
+        }
 
         public void Connect(string targetAddress)
         {
+            _playClientService.Connect(NetworkAddress.Parse(targetAddress));
+            Status = "Connected";
         }
     }
 }
