51b4414b1f77b5b755808bf1521f9c1e0da18acd
Aleksbgbg <kozichka01@abv.bg>
2018-10-17 17:30:00 +0100

    [HostServer] Load machine info on separate thread, displaying loading indicator in the meantime.

--- SharePlay/ViewModels/HostServerViewModel.cs
+++ SharePlay/ViewModels/HostServerViewModel.cs
@@ -1,16 +1,41 @@
 ﻿namespace SharePlay.ViewModels
 {
     using System.Net;
+    using System.Threading.Tasks;
 
     using Caliburn.Micro;
 
     using SharePlay.Models;
+    using SharePlay.Services.Interfaces;
     using SharePlay.ViewModels.Interfaces;
 
     internal class HostServerViewModel : ViewModelBase, IHostServerViewModel
     {
-        public NetworkAddress HostAddress { get; }
+        public HostServerViewModel(INetworkService networkService)
+        {
+            Task.Run(async () => await networkService.ConfigureMachineForHosting().ContinueWith(task =>
+            {
+                HostAddress = new NetworkAddress(networkService.ExternalIp, 3555);
+                IsLoading = false;
+            }));
+        }
+
+        public NetworkAddress HostAddress { get; private set; }
 
         public IObservableCollection<IPAddress> ConnectedUsers { get; } = new BindableCollection<IPAddress>();
+
+        private bool _isIsLoading = true;
+        public bool IsLoading
+        {
+            get => _isIsLoading;
+
+            private set
+            {
+                if (_isIsLoading == value) return;
+
+                _isIsLoading = value;
+                NotifyOfPropertyChange(() => IsLoading);
+            }
+        }
     }
 }
