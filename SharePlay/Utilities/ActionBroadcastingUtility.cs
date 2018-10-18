8fb8bc4a050e335b3e73b00f79619210c7000bea
Aleksbgbg <kozichka01@abv.bg>
2018-10-18 17:31:08 +0100

    [ActionBroadcastingUtility] Add instanced utility which will subscribe to MediaPlayerService events, and broadcast them using the appropriate method.

---
+++ SharePlay/Utilities/ActionBroadcastingUtility.cs
@@ -0,0 +1,15 @@
+﻿namespace SharePlay.Utilities
+{
+    using System;
+
+    using SharePlay.Services.Interfaces;
+
+    internal class ActionBroadcastingUtility
+    {
+        internal void BroadcastActions(IMediaPlayerService mediaPlayerService, Action<string> broadcastMethod)
+        {
+            mediaPlayerService.Played += (sender, e) => broadcastMethod("Play");
+            mediaPlayerService.Paused += (sender, e) => broadcastMethod("Pause");
+        }
+    }
+}
