1a08157879f5136be8690a458325f28d6130250d
Aleksbgbg <kozichka01@abv.bg>
2018-10-18 17:44:15 +0100

    [ActionBroadcastingUtility] Hold an instance of MediaPlayerService, and delegate actions to appropriate methods using reflection.

--- SharePlay/Utilities/ActionBroadcastingUtility.cs
+++ SharePlay/Utilities/ActionBroadcastingUtility.cs
@@ -1,15 +1,43 @@
 ﻿namespace SharePlay.Utilities
 {
     using System;
+    using System.Linq;
+    using System.Reflection;
+
+    using Caliburn.Micro;
 
     using SharePlay.Services.Interfaces;
 
     internal class ActionBroadcastingUtility
     {
-        internal void BroadcastActions(IMediaPlayerService mediaPlayerService, Action<string> broadcastMethod)
+        private readonly IMediaPlayerService _mediaPlayerService;
+
+        private readonly MethodInfo[] _playerMethods;
+
+        internal ActionBroadcastingUtility(IMediaPlayerService mediaPlayerService)
         {
-            mediaPlayerService.Played += (sender, e) => broadcastMethod("Play");
-            mediaPlayerService.Paused += (sender, e) => broadcastMethod("Pause");
+            _mediaPlayerService = mediaPlayerService;
+
+            _playerMethods = mediaPlayerService.GetType().GetMethods();
+        }
+
+        internal void BroadcastAllActions(Action<string> broadcastMethod)
+        {
+            _mediaPlayerService.Played += (sender, e) => broadcastMethod("Play");
+            _mediaPlayerService.Paused += (sender, e) => broadcastMethod("Pause");
+        }
+
+        internal void ReceiveAction(string action)
+        {
+            string[] commands = action.Split(' ');
+
+            string methodName = commands[0];
+
+            object[] arguments = commands.Length == 1 ? null : commands.Skip(1).Cast<object>().ToArray();
+
+            MethodInfo targetMethod = _playerMethods.Single(method => method.Name == methodName);
+
+            Execute.BeginOnUIThread(() => targetMethod.Invoke(_mediaPlayerService, arguments));
         }
     }
 }
