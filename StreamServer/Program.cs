6d3b9900d711d2a7b115cee89e972b5ee854a207
Aleksbgbg <kozichka01@abv.bg>
2018-10-19 09:08:10 +0100

    [Program] Use constants to define StreamServer URL.

--- StreamServer/Program.cs
+++ StreamServer/Program.cs
@@ -3,12 +3,15 @@
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
 
+    using Shared.Configuration;
+
     internal static class Program
     {
         private static void Main(string[] args)
         {
             WebHost.CreateDefaultBuilder(args)
                    .UseStartup<Startup>()
+                   .UseUrls($"http://localhost:{Constants.StreamServerPort}")
                    .Build()
                    .Run();
         }
