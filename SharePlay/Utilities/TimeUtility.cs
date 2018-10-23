c33aa6ad3cde64e392b46aac1451ef6e06452d9e
Aleksbgbg <kozichka01@abv.bg>
2018-10-23 21:25:13 +0100

    [TimeUtility] Add class which manages a time difference with a server.

---
+++ SharePlay/Utilities/TimeUtility.cs
@@ -0,0 +1,26 @@
+﻿namespace SharePlay.Utilities
+{
+    using System;
+    using System.Diagnostics;
+
+    internal static class TimeUtility
+    {
+        private static readonly Stopwatch Stopwatch = new Stopwatch();
+
+        private static TimeSpan syncOffset;
+
+        internal static TimeSpan TimeSinceSyncEpoch => Stopwatch.Elapsed + syncOffset;
+
+        internal static void Start()
+        {
+            Stopwatch.Start();
+        }
+
+        internal static void SyncWithOffset(TimeSpan epoch)
+        {
+            Stopwatch.Reset();
+            Stopwatch.Start();
+            syncOffset = epoch;
+        }
+    }
+}
