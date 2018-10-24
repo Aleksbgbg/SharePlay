918f8d0b2e8f7a2721ec18996ef368de5f76d159
Aleksbgbg <kozichka01@abv.bg>
2018-10-24 09:32:15 +0100

    [MockRequestFilter] Add middleware to filter out stream requests from Windows-Media-Player user-agents.

---
+++ StreamServer/Middleware/MockRequestFilter.cs
@@ -0,0 +1,26 @@
+﻿namespace StreamServer.Middleware
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+
+    public class MockRequestFilter
+    {
+        private readonly RequestDelegate _next;
+
+        public MockRequestFilter(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Headers["User-Agent"].ToString().StartsWith("Windows-Media-Player"))
+            {
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
