d7328f10b4dc9ba39e404775a1fdce08d491531d
Aleksbgbg <kozichka01@abv.bg>
2018-10-24 09:34:43 +0100

    [ApplicationBuilderExtensions] Add extension to register MockRequestFilter.

---
+++ StreamServer/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace StreamServer.Extensions
+{
+    using Microsoft.AspNetCore.Builder;
+
+    using StreamServer.Middleware;
+
+    internal static class ApplicationBuilderExtensions
+    {
+        internal static IApplicationBuilder UseMockRequestFilter(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<MockRequestFilter>();
+        }
+    }
+}
