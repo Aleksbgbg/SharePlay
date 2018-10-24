e31e1c7526ca8db5696d1e6af792e06ba218283d
Aleksbgbg <kozichka01@abv.bg>
2018-10-24 09:37:47 +0100

    [IReceiverService] Remove implementation methods, instead exposing a single 'Receive' method which delegates to implementation methods personally.

--- SharePlay/Services/NetworkInteraction/Interfaces/IReceiverService.cs
+++ SharePlay/Services/NetworkInteraction/Interfaces/IReceiverService.cs
@@ -1,19 +1,7 @@
 ﻿namespace SharePlay.Services.NetworkInteraction.Interfaces
 {
-    using System;
-
     internal interface IReceiverService
     {
-        void Progress(TimeSpan value);
-
-        void Speed(double value);
-
-        void Stop();
-
-        void Play();
-
-        void Pause();
-
-        void Load(string url);
+        void Receive(string message);
     }
 }
